5b4d8f73704a7c024b860c8b86785e4e8caad12e
RutujaGChavan <rutujachavan1818@gmail.com>
2023-03-02 20:44:43 +0530

    addition code

---
+++ hello.java
@@ -0,0 +1,17 @@
+class Demo1 {
+
+    void SayHello() {
+        System.out.println("Welcome");
+                
+    }
+}
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Demo1 obj = new Demo1();
+        obj.SayHello();
+
+    }
+
+}
